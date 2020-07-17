856e028f5cab60f848f84d36457ace77d0491be4
Matt DeKrey <mattdekrey@gmail.com>
2020-07-16 19:16:17 -0500

    Add code to unregister old serviceWorker

---
+++ gatsby-frontend/service-worker.js
@@ -0,0 +1,7 @@
+
+// remove the old service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.unregister();
+    });
+  }
