2bf654a56975b9bfee2354ee0fe2fed5d783937f
Matt DeKrey <mattdekrey@gmail.com>
2021-07-17 10:37:13 -0500

    Correct k8s resources

--- gatsby-frontend/src/pages/404.tsx
+++ gatsby-frontend/src/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404'd!</h1>
-    <p>Oops! You bwoke it.</p>
+    <p>Oops! You broke it.</p>
     <p className="text-xs">I really hope this is logged somewhere&hellip;</p>
   </Layout>
 )
