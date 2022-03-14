2d038c0f5b2404529fe02e9381cc494b6c21362a
Matt DeKrey <mattdekrey@gmail.com>
2022-03-14 08:10:52 -0500

    Switch to helm deployments

--- publish-site.ps1
+++ publish-site.ps1
@@ -3,5 +3,16 @@ cd .\gatsby-frontend
 npm run build-docker
 az acr login --name dekreydotnet
 docker push dekreydotnet.azurecr.io/dekreydotnet:latest
-kubectl -n dekrey-dot-net set image deployment dekrey-dot-net web=$(docker inspect --format='{{index .RepoDigests 0}}' dekreydotnet.azurecr.io/dekreydotnet:latest)
+
+$fullImageName = 'dekreydotnet.azurecr.io/dekreydotnet'
+$imageTag = 'latest'
+$sslClusterIssuer = 'letsencrypt'
+
+helm upgrade --install -n dekrey-dot-net dekrey-dot-net --create-namespace mdekrey/single-container `
+    --set-string "image.repository=$($fullImageName)" `
+    --set-string "image.tag=$imageTag,image.pullPolicy=Always" `
+    --set-string "ingress.annotations.cert-manager\.io/cluster-issuer=$sslClusterIssuer" `
+    --set-string "ingress.hosts[0].host=dekrey.net" `
+    --set-string "ingress.hosts[1].host=www.dekrey.net"
+
 Pop-Location
