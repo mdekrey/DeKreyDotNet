e86faa2173008d53b69e6ca258d60aba6bbbb1ff
Matt DeKrey <mattdekrey@gmail.com>
2022-11-19 07:30:15 -0600

    Extract intro block

---
+++ nextjs-frontend/src/components/intro-block.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import fullUrl from 'src/images/full.jpg';
+import styles from './intro-block.module.css';
+import classNames from 'classnames';
+
+import { MdToggleOff } from 'react-icons/md';
+
+export function IntroBlock() {
+	return (
+		<div
+			className={classNames(
+				'flex flex-col items-center md:flex-row md:items-start md:justify-center max-h-screen overflow-hidden mx-auto max-w-4xl sticky top-0 px-4 md:px-8',
+				styles['post-blur']
+			)}>
+			<div className="flex flex-col flex-shrink-0 mt-8 md:mt-[20%]">
+				<h1 className="text-2xl md:text-left font-bold">
+					Hey, <br />
+					<span className="text-purple-700 text-4xl">I'm Matt DeKrey!</span>
+				</h1>
+				<p className="text-xs">MAT deh-KRAY</p>
+				<p className="text-xs">/mæt d&#x026A;&#x02c8;kre&#x026A;/</p>
+				<p className="text-xs">he/him</p>
+
+				<p className="text-sm">
+					<MdToggleOff className="text-red-700 text-[36px] inline-block" /> Not available for contracting
+				</p>
+			</div>
+			<div
+				className="max-w-screen md:flex-shrink-default bg-contain bg-no-repeat h-[100vmin] w-[100vmin] md:h-[60vmin] md:w-[80vmin]"
+				style={{
+					backgroundImage: `url(${fullUrl.src})`,
+					backgroundPosition: '50% botom',
+				}}
+			/>
+		</div>
+	);
+}
--- nextjs-frontend/src/pages/index.tsx
+++ nextjs-frontend/src/pages/index.tsx
@@ -4,10 +4,8 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { GetStaticProps, GetStaticPropsResult } from 'next/types';
 import { BlogPost, getAllPosts } from '../articles/utils';
-import fullUrl from 'src/images/full.jpg';
-import styles from './index.module.css';
-import classNames from 'classnames';
 import { BlogPostSummaryDisplay } from 'src/components/blog-post-summary-display';
+import { IntroBlock } from 'src/components/intro-block';
 
 type BlogPostSummary = Pick<BlogPost, 'slug' | 'frontmatter'>;
 
@@ -21,28 +19,7 @@ const IndexPage = ({ posts }: IndexProps) => {
 			<SEO title="Home" />
 
 			<Layout.NoScrollHeaderSlot>
-				<div
-					className={classNames(
-						'flex flex-col items-center md:flex-row md:items-start md:justify-center max-h-screen overflow-hidden mx-auto max-w-4xl sticky top-0 pt-16 px-4 md:px-8',
-						styles['post-blur']
-					)}>
-					<div className="flex flex-col flex-shrink-0 mt-8 md:mt-[20%]">
-						<h1 className="text-2xl md:text-left font-bold">
-							Hey, <br />
-							<span className="text-purple-700 text-4xl">I'm Matt DeKrey!</span>
-						</h1>
-						<p className="text-xs">MAT deh-KRAY</p>
-						<p className="text-xs">/mæt d&#x026A;&#x02c8;kre&#x026A;/</p>
-						<p className="text-xs">he/him</p>
-					</div>
-					<div
-						className="max-w-screen md:flex-shrink-default bg-contain bg-no-repeat h-[100vmin] w-[100vmin] md:h-[60vmin] md:w-[80vmin]"
-						style={{
-							backgroundImage: `url(${fullUrl.src})`,
-							backgroundPosition: '50% 20%',
-						}}
-					/>
-				</div>
+				<IntroBlock />
 			</Layout.NoScrollHeaderSlot>
 
 			<div className="divide-y -my-8">
