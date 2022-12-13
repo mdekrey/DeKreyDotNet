db53ce60d4f24b159641a7a2ad27d63c18194b3e
Matt DeKrey <mattdekrey@gmail.com>
2022-12-13 11:21:28 -0600

    Add source link capability

--- nextjs-frontend/src/siteMetadata.ts
+++ nextjs-frontend/src/siteMetadata.ts
@@ -3,4 +3,5 @@ export default {
 	description: `Personal articles and profile of Matt DeKrey`,
 	author: `@mdekrey`,
 	url: process.env.NEXT_PUBLIC_DOMAIN ?? `https://example.com`,
+	gitHash: process.env.NEXT_PUBLIC_GIT_HASH ?? `main`,
 } as const;
