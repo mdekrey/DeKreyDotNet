ed362fcafbe2bbbc4bc024e01a0f37481be33707
Matt DeKrey <mattdekrey@gmail.com>
2023-02-08 06:50:45 -0600

    Corrections to mobile header
    Started adding a mobile menu, but realized I didn't need an extra menu
    when I ran into trouble with Astro's client hydration.

---
+++ ui/src/components/MenuToggle.tsx
@@ -0,0 +1,35 @@
+import { OpenMobileMenu, useMobileMenuState } from '@/core/mobile-menu-state';
+import { twMerge } from 'tailwind-merge';
+
+const MenuToggle = ({
+	mobileMenu,
+	children,
+}: {
+	mobileMenu: OpenMobileMenu;
+	children?: React.ReactNode;
+}) => {
+	const [currentMobileMenu, setMobileMenu] = useMobileMenuState();
+
+	const isActive = currentMobileMenu === mobileMenu;
+
+	return (
+		<button
+			className={twMerge(
+				'border rounded-md border-transparent bg-transparent p-2 md:hidden',
+				isActive && 'border-slate-500 bg-slate-200'
+			)}
+			type="button"
+			aria-pressed={isActive ? 'true' : 'false'}
+			onClick={toggleMenu}
+		>
+			{children}
+		</button>
+	);
+
+	function toggleMenu() {
+		console.log('toggle menu', isActive ? null : mobileMenu);
+		setMobileMenu(isActive ? null : mobileMenu);
+	}
+};
+
+export default MenuToggle;
