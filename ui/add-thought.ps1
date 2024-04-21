591aed6504e65461c5c0931419a01b013097bce4
Matt DeKrey <mattdekrey@gmail.com>
2024-04-21 18:19:42 -0500

    Add thought and fix mobile layout

--- ui/add-thought.ps1
+++ ui/add-thought.ps1
@@ -2,6 +2,11 @@ Push-Location $PSScriptRoot
 
 $path = "src/thoughts/$(Get-Date -Format 'yyyy-MM/ddTHHmmss').mdx"
 $null = New-Item -ItemType File -Name $path -Force
+@'
+---
+tags: []
+---
+'@ > $path
 
 code $path
 
