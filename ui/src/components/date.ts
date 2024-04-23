60d302bebed9f9d64dececc3ad09de98b67cff0e
Matt DeKrey <mattdekrey@gmail.com>
2024-04-23 07:10:36 -0500

    Add support for relative dates
    Fix blog summary layout

--- ui/src/components/date.ts
+++ ui/src/components/date.ts
@@ -1,9 +1,28 @@
-import { formatRelative } from 'date-fns';
+import { formatRelative, parse } from 'date-fns';
 
-// Converts `data-date={unixMs}` into time-ago text
-const elems = document.querySelectorAll('[data-date]') as Iterable<HTMLElement>;
-for (const elem of elems) {
-	elem.classList.remove('invisible');
-	elem.title = elem.innerText;
-	elem.innerHTML = formatRelative(Number(elem.dataset.date), new Date(), {});
+// Converts `data-datetime={unixMs}` into a relative date
+export function applyDateTime() {
+	const elems = document.querySelectorAll<HTMLElement>('[data-datetime]');
+	for (const elem of elems) {
+		elem.classList.remove('invisible');
+		elem.title = elem.innerText;
+		elem.innerHTML = formatRelative(
+			Number(elem.dataset.datetime),
+			new Date(),
+			{},
+		);
+	}
+}
+
+export function applyDate() {
+	const elems = document.querySelectorAll<HTMLElement>('[data-date]');
+	for (const elem of elems) {
+		elem.classList.remove('invisible');
+		elem.title = elem.innerText;
+		elem.innerHTML = formatRelative(
+			parse(elem.dataset.date!, 'yyyy-MM-dd', new Date()),
+			new Date(),
+			{},
+		);
+	}
 }
